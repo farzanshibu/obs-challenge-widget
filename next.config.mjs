fa1915f47c99f9d31cfbf4c6555cf5a8670c4142
Farzan Shibu <farzanshibu786@gmail.com>
2024-05-20 05:35:55 +0530

    chore: Update .gitignore and Dockerfile, delete unused files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// // export default nextConfig;
+// export function webpack(config, options) {
+//     const { isServer } = options;
+//     config.module.rules.push({
+//         test: /\.(ogg|mp3|wav|mpe?g)$/i,
+//         exclude: config.exclude,
+//         use: [
+//             {
+//                 loader: require.resolve('url-loader'),
+//                 options: {
+//                     limit: config.inlineImageLimit,
+//                     fallback: require.resolve('file-loader'),
+//                     publicPath: `${config.assetPrefix}/_next/static/images/`,
+//                     outputPath: `${isServer ? '../' : ''}static/images/`,
+//                     name: '[name]-[hash].[ext]',
+//                     esModule: config.esModule || false,
+//                 },
+//             },
+//         ],
+//     });
+
+//     return config;
+// }
+//const withCSS = require('@zeit/next-css');
+import compose from 'next-compose';
+//cssConfig = {};
+export default compose([
+//[withCSS, cssConfig],
+{
+webpack(config, options) {
+config.module.rules.push({
+test: /.mp3$/,
+use: {
+loader: 'file-loader',
+},
+});
+return config;
+},
+},
+]);
