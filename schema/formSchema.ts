c9bcfbec9bdbe2afb188e1e918a4868459aee672
Farzan Shibu <farzanshibu786@gmail.com>
2024-05-20 05:37:46 +0530

    chore: Setup formSchema to allow nonnegative values for currentValue

--- schema/formSchema.ts
+++ schema/formSchema.ts
@@ -5,7 +5,7 @@ export const formSchema = z.object({
     maxValue: z.preprocess((a) => parseInt(z.string().parse(a), 10),
         z.number().int().positive()),
     currentValue: z.preprocess((a) => parseInt(z.string().parse(a), 10),
-        z.number().int().positive()),
+        z.number().int().nonnegative()),
     endDate: z.date().optional(),
     is_active: z.boolean(),
 });
